9e1af73bb2972d0b414dff49687622045dfcb495
Adam M Jensiki <adam.jeniski@ibm.com>
2020-09-20 23:21:14 -0400

    init playground files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <h1 className="center">Test</h1>
+      <p className="center">This is my first application</p>
     </div>
   );
 }
